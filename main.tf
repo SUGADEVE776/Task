69512dc789dd3ef157124a405d2f0ac79d836d5d
root <root@ip-172-31-43-205.us-east-2.compute.internal>
2022-11-06 13:29:15 +0000

    modules

--- main.tf
+++ main.tf
@@ -1,12 +1,39 @@
 resource "aws_instance" "instance_1" {
-  ami           = var.instance_ami
-  instance_type = var.instance_type
-  key_name	= var.ec2_keypair
+  ami           = var.instance_1_ami
+  instance_type = var.instance_1_type
   tags = {
-    Name = var.instance_name
+    Name = var.instance_1_name
+  }
+}
+
+resource "aws_instance" "instance_2" {
+  ami           = var.instance_2_ami
+  instance_type = var.instance_2_type
+  tags = {
+    Name = var.instance_2_name
   }
   provisioner "local-exec" {
     command    = "echo The IP address of the Server is ${self.private_ip}"
     on_failure = continue
+  }
+}
+module "website_s3_bucket" {
+  source = "./modules/aws-s3-static-website-bucket"
+
+  bucket_name = "clickittech-terraform-best-practices"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "test"
+  }
+}
+module "website_s3_bucket_2" {
+  source = "./modules/aws-s3-static-website-bucket"
+
+  bucket_name = "clickittech-terraform-best-practices-bucket-2"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "test"
   }
 }
--- output.tf
+++ output.tf
@@ -1,4 +1,23 @@
-output "instance_1" {
+output "instance_1_id" {
   description = "The ID of the instance-1"
   value       = try(aws_instance.instance_1.id)
+}
+
+output "instance_2_id" {
+  description = "The ID of the instance-2"
+  value       = try(aws_instance.instance_2.id)
+}
+output "website_bucket_2_arn" {
+  description = "ARN of the bucket"
+  value       = module.website_s3_bucket_2.arn
+}
+
+output "website_bucket_2_name" {
+  description = "Name (id) of the bucket"
+  value       = module.website_s3_bucket_2.name
+}
+
+output "website_bucket_2_domain" {
+  description = "Domain name of the bucket"
+  value       = module.website_s3_bucket_2.domain
 }
